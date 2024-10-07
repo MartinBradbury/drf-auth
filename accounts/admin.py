4b0ebb27e2495876253e1a4486b0717c02e1c66f
Martin Bradbury <martinbradbury@btinternet.com>
2024-10-07 13:22:48 +0100

    created the form.py with custom user creation form and customuserchangeform along with registering customuserauth in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUserAuth
+from .forms import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(CustomUserAuth)
+class CustomAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    
+    model = CustomUserAuth
+    
